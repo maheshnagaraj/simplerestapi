a54a45797c74411aee324404b48e18d9206ab407
Nagaraj <i868208@c02w3fuhhtd6.dhcp.pal.sap.corp>
2018-05-03 18:31:14 -0700

    Sample python rest api template using Flask

---
+++ restapi.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/api/v1.0/collectFacts', methods=['POST'])
+def collectFacts():
+    """
+    """
+    inputDataPayload = request.json
+    return jsonify(inputDataPayload)
+   
+if __name__ == '__main__':
+    app.run(debug=True)
